298136560f4214b186ef532dfd95e02ad9587460
Kian Nassre <knassre@macbook-pro-403.wifi.local.cmu.edu>
2022-10-30 17:12:08 -0400

    Initial commit: designing fixtures and new test

--- app/tests/test_routes.py
+++ app/tests/test_routes.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import pytest
+import pandas as pd
 
 from app.handlers.routes import configure_routes
 
@@ -13,3 +15,44 @@ def test_base_route():
 
     assert response.status_code == 200
     assert response.get_data() == b'try the predict route it is great!'
+
+    
+    
+@pytest.fixture(
+    params=[
+        pytest.param(
+            pd.DataFrame({ 'age' : pd.Series([1, 10, 100]) ,'health' : pd.Series([15, 18, 4]) ,'absences' : pd.Series([10, 0, 4]), "G3": pd.Series([5, 18, 3])}),
+            id="testA"
+        ),
+        pytest.param(
+            pd.DataFrame({ 'age' : pd.Series([1, 10, 100]) ,'health' : pd.Series([15, 18, 4]) ,'absences' : pd.Series([10, 0, 4]), "G3": pd.Series([12, 13, 8])}),
+            id="testB"
+        )
+    ]
+)
+def training_data(request):
+    return request.param
+    
+@pytest.fixture
+def acceptable_ratio():
+    return 0.75
+    
+def test_dummy(training_data, acceptable_ratio):
+    app = Flask(__name__)
+    configure_routes(app)
+    client = app.test_client()
+    
+    inputs = pd.DataFrame({...})
+    
+    correct = 0
+    total = 0
+    
+    for _, row in training_data.iterrows():
+        exp = 1 if row["G3"] >= 15 else 0
+        url = '/predict'
+        response = client.get(url, query_string={'age': row["age"], "health": row["health"], "absences": row["absences"]})
+        assert response.status_code == 200
+        total += 1
+        correct += int(response.get_data()) == exp
+        
+    assert correct / total >= acceptable_ratio
